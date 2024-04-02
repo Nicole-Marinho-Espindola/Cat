5b864abd2b8980b8cbc45c02b0f2f5cb72d5c558
Nicole-Marinho-Espindola <nicole.m.espindola@gmail.com>
2024-04-01 22:03:35 -0300

    initializing the environment

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+sass.compiler = require('node-sass')
+
+gulp.task('default', watchSass)
+gulp.task('sass', compileSass)
+
+function compileSass() {
+    return gulp
+        .src('assets/sass/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('assets/css'));
+  };
+
+function watchSass(){
+gulp.watch('assets/sass/*.scss', compileSass)
+}
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Cat</title>
+</head>
+<body>
+    <div class="container">
+        <div class="floater">
+            <div class="face"></div>
+            <div class="ear ear-1"></div>
+            <div class="ear ear-2"></div>
+            <div class="eye eye-1"></div>
+            <div class="pupil pupil-1"></div>
+            <div class="sparkle sparkle-1"></div>
+            <div class="sparkle sparkle-2"></div>
+            <div class="eye eye-2"></div>
+            <div class="pupil pupil-1"></div>
+            <div class="sparkle sparkle-3"></div>
+            <div class="sparkle sparkle-4"></div>
+            <div class="whisker-1"></div>
+            <div class="whisker-2"></div>
+            <div class="whisker-3"></div>
+            <div class="whisker-4"></div>
+            <div class="whisker-5"></div>
+            <div class="whisker-6"></div>
+            <div class="nose nose-1"></div>
+            <div class="nose nose-2"></div>
+            <div class="nose-triangle"></div>
+            <div class="tongue"></div>
+            <div class="feet feet-1">
+                <div class="nail nail-1"></div>
+                <div class="nail nail-2"></div>
+                <div class="nail nail-3"></div>
+            </div>
+            <div class="feet feet-2">
+                <div class="nail nail-1"></div>
+                <div class="nail nail-2"></div>
+                <div class="nail nail-3"></div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
